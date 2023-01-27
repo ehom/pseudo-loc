9d76503bd0d66baa9087dd07329898010059f6c6
ehom <ehom@users.noreply.github.com>
2023-01-26 16:57:06 -0800

    test default localizer

--- pseudo_loc/lib/test_localizer.py
+++ pseudo_loc/lib/test_localizer.py
@@ -1,6 +1,6 @@
 import unittest
-from localizer import Localizer
-from localizer import UnicodeChars
+from pseudo_loc.lib.localizer import Localizer
+from pseudo_loc.lib.localizer import UnicodeChars
 
 
 class MyTestCase(unittest.TestCase):
@@ -8,6 +8,17 @@ class MyTestCase(unittest.TestCase):
         self.localizer = Localizer()
 
     def test_default_localizer(self):
+        localized = self.localizer.localize("abc")
+        localized = localized.encode('utf-8')
+        expected = UnicodeChars.MATH_LEFT_DOUBLE_ANGLE + \
+            "abc" + \
+            UnicodeChars.MATH_RIGHT_DOUBLE_ANGLE
+        expected = expected.encode('utf-8')
+
+        self.assertEqual(localized, expected)
+
+    def test_localizer_with_padding(self):
+        self.localizer.pad_text = True
         localized = self.localizer.localize("abc")
         localized = localized.encode('utf-8')
         expected = UnicodeChars.MATH_LEFT_DOUBLE_ANGLE + \
